31ef1fd20bdc9d8ac19aff34cbf12a4b8ad2df1c
turusuke <mail@turusuke.com>
2016-05-23 03:47:37 +0900

    Alfred 3 で 2バイト文字を入力した時に返す値がおかしくなっていたのを修正

--- dev/index.php
+++ dev/index.php
@@ -13,7 +13,7 @@ function getURL($query){
 
     $query = htmlspecialchars($query ,ENT_QUOTES, 'UTF-8');
 
-    $crawler = $client->request('GET', 'http://kaomojiya.jp/keyword/'.$query.'/');
+    $crawler = $client->request('GET', 'http://kaomojiya.jp/keyword/'.urlencode($query).'/');
 
     $obj = $crawler->filter('.detailArea ul')->each(function($node){
         $ver  = $node->filter('li.whiteArea')->text();
